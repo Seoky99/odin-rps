8a0d6d855653343e45da81dd09367e1cb17df629
sh845@cornell.edu <seokyoung99@gmail.com>
2022-02-27 02:51:58 -0500

    Init rps

--- rps.js
+++ rps.js
@@ -1,63 +1,112 @@
 let playerWins = 0;
-let computerWins = 0; 
+let computerWins = 0;
 let draws = 0;
 
 function computerPlay() {
-    let result; 
-    let compValue = computerRoll(); 
+    let result;
+    let compValue = computerRoll();
     switch (compValue) {
-        case 0: 
+        case 0:
             result = 'Rock';
-            break; 
-        case 1: 
+            break;
+        case 1:
             result = 'Paper';
-            break; 
-        case 2: 
-            result = 'Scissors'; 
-            break; 
+            break;
+        case 2:
+            result = 'Scissors';
+            break;
     }
-    return result; 
+    return result;
 }
 
 function computerRoll() {
-    return Math.floor(Math.random() * 3); 
+    return Math.floor(Math.random() * 3);
 }
+
+let buttons = document.querySelectorAll('.btn');
+let cmpbuttons = document.querySelectorAll('.cmpbtn');
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+
+
+        buttons.forEach((button) => {
+            button.style.backgroundColor = button.style.border;
+        })
+
+        cmpbuttons.forEach((button) => {
+            button.style.backgroundColor = button.style.border;
+        })
+
+        let pSelect = e.target.classList[1];
+        let cSelect = computerPlay();
+        let newText = playRound(pSelect, cSelect);
+
+        //set other choices to grey
+        buttons.forEach((button) => {
+            if (button.classList[1] != pSelect) {
+                button.style.backgroundColor = 'black';
+            }
+        })
+
+        cmpbuttons.forEach((button) => {
+            if (button.classList[1] != cSelect.toLowerCase()) {
+                button.style.backgroundColor = 'black';
+            }
+        })
+
+
+
+        if (playerWins + computerWins + draws < 5) {
+
+            if (document.querySelector(".score") == null) {
+
+                let newElt = document.createElement('h3');
+
+                newElt.textContent = newText;
+                newElt.setAttribute('style', 'color: white; text-align:center;');
+                newElt.classList.add('score');
+                let cont = document.querySelector('.container2');
+                cont.appendChild(newElt);
+
+            } else {
+                let elt = document.querySelector(".score");
+                elt.textContent = newText;
+            }
+        } else {
+            let elt = document.querySelector(".score");
+            elt.textContent = winner(playerWins, computerWins, draws)
+
+            resetGame();
+        }
+    })
+})
 
 function playRound(playerSelection, computerSelection) {
     let sanitized = sanitizeInput(playerSelection);
-    let playerChoice = sanitized.charAt(0).toUpperCase() + sanitized.slice(1); 
+    let playerChoice = sanitized.charAt(0).toUpperCase() + sanitized.slice(1);
 
     if (playerChoice === computerSelection) {
-        draws++; 
+        draws++;
         return `You Tied! ${playerChoice} ties with ${computerSelection}!`
     } else if (playerLoses(playerChoice, computerSelection)) {
-        computerWins++; 
+        computerWins++;
         return `You Lost! ${computerSelection} beats ${playerChoice}!`
     } else {
-        playerWins++; 
+        playerWins++;
         return `You Won! ${playerChoice} beats ${computerSelection}!`
     }
 }
 
 function playerLoses(playerChoice, computerSelection) {
     return ((playerChoice === 'Rock' && computerSelection === 'Paper') ||
-    (playerChoice === 'Paper' && computerSelection === 'Scissors') ||
-    (playerChoice === 'Scissors' && computerSelection === 'Rock'))
+        (playerChoice === 'Paper' && computerSelection === 'Scissors') ||
+        (playerChoice === 'Scissors' && computerSelection === 'Rock'))
 }
 
-function sanitizeInput(playerSelection){
-    return playerSelection.toLowerCase(); 
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerInput = prompt("Rock, Paper, or Scissors?"); 
-        let computerSelection = computerPlay(); 
-        let result = playRound(playerInput, computerSelection); 
-        console.log(result); 
-    }   
-    console.log(winner(playerWins, computerWins, draws));
-    resetGame(); 
+function sanitizeInput(playerSelection) {
+    return playerSelection.toLowerCase();
 }
 
 function winner(w, l, d) {
@@ -74,7 +123,7 @@ function winner(w, l, d) {
 }
 
 function resetGame() {
-    computerWins = 0; 
-    draws = 0; 
-    playerWins = 0; 
+    computerWins = 0;
+    draws = 0;
+    playerWins = 0;
 }
